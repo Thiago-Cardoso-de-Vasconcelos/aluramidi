1aae114fc2cdb9de025b701349bead8fe3ee7dea
Thiago Cardoso de Vasconcelos <thiagocardosodevasconcelos@outlook.com>
2023-01-10 00:43:52 -0300

    for

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 function tocaSom (idElementoAudio) {
     document.querySelector(idElementoAudio).play();
-};
+}
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
-
-while (contador < listaDeTeclas.length) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
      const idAudio = `#som_${instrumento}`;
@@ -14,5 +12,5 @@ while (contador < listaDeTeclas.length) {
     listaDeTeclas[contador].onclick =  function() {
         tocaSom(idAudio);
     }    
-    contador = contador + 1;
-};
+   
+}
